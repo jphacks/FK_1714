f115f0d070a1a034863cd3e3ef9bf0b5957761b6
daiki <daiki@tetrabookS.local>
2017-10-20 21:21:39 +0900

    daiki make directory

---
+++ negoto_daiki/negoto_daikiTests/negoto_daikiTests.swift
@@ -0,0 +1,36 @@
+//
+//  negoto_daikiTests.swift
+//  negoto_daikiTests
+//
+//  Created by daiki on 2017/10/20.
+//  Copyright © 2017 daiki. All rights reserved.
+//
+
+import XCTest
+@testable import negoto_daiki
+
+class negoto_daikiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
